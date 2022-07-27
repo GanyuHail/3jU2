d24041c051f25b16675eaf18301e55567703e3fc
HailGanyu <henryyanguk@gmail.com>
2022-07-27 17:00:26 +0100

    sphere sizing

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
     spotLight.position.set(0, 64, 32);
     scene.add(spotLight);
 
-    const sphereGeometry = new THREE.SphereGeometry(30, 15, 10);
+    const sphereGeometry = new THREE.SphereGeometry(12, 64, 32);
     const cubeTexture = new THREE.TextureLoader().load( 'https://raw.githubusercontent.com/GanyuHail/3dUni/af0410ab94f43f47c605838cd9db4d380639afb7/src/assets/uni.jpg' );
     const boxMaterial = new THREE.MeshBasicMaterial( { map: cubeTexture } );
     const boxMesh = new THREE.Mesh(sphereGeometry, boxMaterial);
